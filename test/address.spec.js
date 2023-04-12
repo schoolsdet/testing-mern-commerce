3b1c124c03f0756e0f9675fbf3b8ecb78731724d
Baurz <baurzhan.zh@gmail.com>
2023-04-11 21:50:28 -0400

    Added address

--- test/address.spec.js
+++ test/address.spec.js
@@ -1,3 +1,8 @@
+const chai = require('chai');
+const chaiSubset = require('chai-subset');
+chai.use(chaiSubset);
+const expect  =chai.expect;
+const casual = require('casual');
 const superagent = require('superagent');
 
 const baseUrl = "https://mern-ecommerce.sdet.school/api";
@@ -19,7 +24,43 @@ describe("Test address endpoints", ()=> {
           }
     });
     
-    it("should run test",()=>{
-        console.log(token);
-    })
+    it("should add address to user",async () =>{
+        const {street,city,state} = casual;
+        // const street = casual.street;
+        // const city = casual.city;
+        // const state = casual.state;
+        const zip = casual.zip(5)
+        const addressOpt = {
+          isDefault: true,
+          address: street,
+          city: city,
+          state: state,
+          country: "US",
+          zipCode: zip,
+        }
+        let response
+        try {
+          response = await superagent.post(baseUrl+"/address/add")
+          .set({
+              Authorization: token
+            })
+            .send(addressOpt)
+        } catch(err){
+          console.log(err.message)
+        }
+        expect(response.body).to.containSubset({
+          success: true,
+          message: 'Address has been added successfully!',
+          address: {
+            isDefault: true,
+            address: street,
+            city: city,
+            state: state,
+            country: 'US',
+            zipCode: zip,
+            user: '6434b87a3ea9a80035112c5f',
+            __v: 0
+          }
+        });
+    });
 });
