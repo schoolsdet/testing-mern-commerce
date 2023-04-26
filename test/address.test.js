7aca981bb1ab87338c08c9c2bc2ee5e121323409
Baurz <baurzhan.zh@gmail.com>
2023-04-25 22:23:08 -0400

    Playwright

--- test/address.test.js
+++ test/address.test.js
@@ -1,8 +1,8 @@
 
-describe('this is a jest test example', () => {
-  test('example', () => {
-    expect(1).toBe(1);
-    console.log('hello');
-  });
-});
+// describe.skip('this is a jest test example', () => {
+//   test('example', () => {
+//     expect(1).toBe(1);
+//     console.log('hello');
+//   });
+// });
 
--- test/simple_test.spec.js
+++ test/simple_test.spec.js
@@ -1,5 +1,38 @@
+const { expect } = require('chai');
+const browsers = require('playwright');
+const username = 'Jerome20@hotmail.com';
+const password = 'Password12';
+
+const sleep = async (ms) => { return new Promise((resolve) => { return setTimeout(resolve, ms); }); };
+
 describe('Very simple test', () => {
   it('should make a simple test', async() => {
-        
+    const browserType = browsers['webkit'];
+
+    const browser = await browserType.launch({
+      headless: false
+    });
+    console.log(browser.version());
+    const context = await browser.newContext({
+      viewport:{
+        width: 1200,
+        height: 800
+      }
+    });
+    const page = await context.newPage();
+    await page.goto('https://mern-ecommerce.sdet.school/');
+    await page.getByText('Welcome!').click();
+    await page.getByText('Login').click();
+    await page.locator('(//*[@name="email"])[1]').fill(username);
+    await page.locator('(//*[@name="password"])[1]').fill(password);
+    await page.locator('(//*[@type="submit"])[1]').click();
+
+    const element = await page.locator('//*[@class="role member "]');
+    expect(await element.textContent()).to.equal('Member');
+
+    await sleep(5000);
+    page.close();
+    browser.close();
+
   });
 });
