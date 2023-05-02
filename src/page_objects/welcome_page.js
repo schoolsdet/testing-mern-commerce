147e66f14334e1a5962d33cf411636ac89275d74
Baurz <baurzhan.zh@gmail.com>
2023-05-01 22:09:20 -0400

    cart test

--- src/page_objects/welcome_page.js
+++ src/page_objects/welcome_page.js
@@ -1,4 +1,5 @@
 const LoginPage = require('./login_page');
+const ProductPage= require('./product_page');
 
 class WelcomePage {
   constructor(page) {
@@ -11,6 +12,23 @@ class WelcomePage {
     await this.page.getByText('Welcome!').click();
     await this.page.getByText('Login').click();
     return new LoginPage(this.page);
+  }
+  /**
+   * Search for product
+   *
+   * @param {string} productName - a product to search
+   * @param {number} index - a product index
+   */
+  async search(productName, index) {
+    await this.page.locator('//*[@autocomplete="off"]').type(productName);
+    await this.page.locator('ul[role="listbox"]').waitFor();
+    const foundProducts = await this.page.locator('//*[@role="option"]').all();
+
+    if(foundProducts.length-1 <index) {
+      throw new Error('search index out of bounds');
+    }
+    await foundProducts[index].click();
+    return new ProductPage(this.page);        
   }
 }
 
